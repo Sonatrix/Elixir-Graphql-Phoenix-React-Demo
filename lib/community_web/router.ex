23775a4a232d5ad6db0adf8c29c8113cd95a8d61
Praveen Kumar Verma <praveenkumarverma121@gmail.com>
2018-05-23 00:53:35 +0530

    first commit

---
+++ lib/community_web/router.ex
@@ -0,0 +1,19 @@
+defmodule CommunityWeb.Router do
+  use CommunityWeb, :router
+
+  pipeline :api do
+    plug(:accepts, ["json"])
+  end
+
+  scope "/" do
+    pipe_through(:api)
+
+    forward(
+      "/graphql",
+      Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.EndPoint}
+    )
+  end
+end
